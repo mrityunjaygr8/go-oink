d6ddb978d641369b2d7779ac6bf1e2426a382497
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-06-11 18:21:18 +0530

    db folder moved to internal

---
+++ internal/db/migrations/000001_add_user.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "users" (
+  "email" varchar UNIQUE NOT NULL,
+  "id" uuid PRIMARY KEY NOT NULL,
+  "password" varchar NOT NULL,
+  "username" varchar UNIQUE NOT NULL,
+  "created_at" TIMESTAMPTZ NOT NULL,
+  "updated_at" TIMESTAMPTZ NOT NULL
+);
+
