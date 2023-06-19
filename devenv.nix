631ec5dbee389927dc0d1bae08a702441c264cf5
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-06-19 22:11:00 +0530

    using devenv for providing dev environment

---
+++ devenv.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    go-task
+    air
+    go-migrate
+    (callPackage ./sqlboiler.nix { })
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
